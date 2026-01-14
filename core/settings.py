1c052f6a4d1a391085cd6da661c650fe64ec19a2
k3chupe <94481200+k3chupe@users.noreply.github.com>
2026-01-14 22:04:14 +0100

    Refactor database settings to use dj_database_url
    Replaced DATABASES configuration with dj_database_url for better management.

--- core/settings.py
+++ core/settings.py
@@ -85,14 +85,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 
